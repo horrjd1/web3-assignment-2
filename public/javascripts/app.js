56649b67916b49f6cdc31514bb39898d488a0855
horrjd1 <jamie489horrell@gmail.com>
2020-06-18 11:53:06 +1200

    Completed AddCounty form

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -15,12 +15,12 @@ function fetchCountryData() {
     });
   });
 }
-/*
+
 class AddForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formValues: {},
+      formValues: {}
     };
   }
 
@@ -29,52 +29,43 @@ class AddForm extends React.Component {
     let formValues = this.state.formValues;
     let key = event.target.name;
     let value = event.target.value;
-
     formValues[key] = value;
-
-    console.log(event.target.name);
-    console.log(event.target.value);
-
-    this.setState({ formValues });
+    this.setState({
+      formValues
+    });
   }
 
   handleSubmit(event) {
-    event.preventDefault();
-    console.log(this.state.formValues);
+    event.preventDefault(); // Adding empty data key since form doesnt have option to add the json data 
+
+    this.state.formValues.data = {}; // sending request to api to create the country
+
+    fetch("http://localhost:8080/api/countries", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state.formValues)
+    }).then(response => console.log(response)); //provide success feedback
+    //update local storage?
   }
 
   render() {
-    return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <label>
-          {" "}
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={this.state.formValues["name"]}
-            onChange={this.handleChange.bind(this)}
-          />
-        </label>
-        <br />
-        <label>
-          {" "}
-          Data:
-          <input
-            type="text"
-            name="data"
-            value={this.state.formValues["data"]}
-            onChange={this.handleChange.bind(this)}
-          />
-        </label>
-        <br />
-        <input className="btn btn-primary" type="submit" value="Submit" />
-      </form>
-    );
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", null, "Add Country"), /*#__PURE__*/React.createElement("form", {
+      onSubmit: this.handleSubmit.bind(this)
+    }, /*#__PURE__*/React.createElement("label", null, " ", "Name:", /*#__PURE__*/React.createElement("input", {
+      type: "text",
+      name: "name",
+      value: this.state.formValues["name"],
+      onChange: this.handleChange.bind(this)
+    })), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+      className: "btn btn-primary",
+      type: "submit",
+      value: "Submit"
+    })));
   }
-}
-*/
 
+}
 
 class DeleteForm extends React.Component {
   constructor(props) {
@@ -99,8 +90,6 @@ class DeleteForm extends React.Component {
     let key = event.target.name;
     let value = event.target.value;
     formValues[key] = value;
-    console.log(event.target.name);
-    console.log(event.target.value);
     this.setState({
       formValues
     });
@@ -109,8 +98,8 @@ class DeleteForm extends React.Component {
   handleSubmit(event) {
     event.preventDefault(); //Makes the delete request to the site
 
-    fetch('http://localhost:8080/api/countries/' + this.state.formValues['name'], {
-      method: 'DELETE'
+    fetch("http://localhost:8080/api/countries/" + this.state.formValues["name"], {
+      method: "DELETE"
     }).then(response => console.log(response)); //provide success feedback
     //update local storage?
   }
@@ -142,8 +131,6 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    console.log('before data');
-    console.log(this.state.countryData);
     this.loadCountryData();
   }
 
@@ -162,15 +149,13 @@ class App extends React.Component {
     this.setState({
       countryData: data
     });
-    console.log('data');
-    console.log(this.state.countryData);
   }
 
   render() {
     // waits until the data is loaded from the fetch or local storage before loading the delete form
     // this is so that the form isnt loaded and the empty state isnt passed to the delete component before the countryData is set
     if (this.state.countryData) {
-      return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(DeleteForm, {
+      return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(AddForm, null), /*#__PURE__*/React.createElement(DeleteForm, {
         data: this.state.countryData
       }));
     } else {
